6e70e36795e5a3c04b51e9a02bf5f4d1f1ff8f29
vinialberti <albertivini99@gmail.com>
2023-07-17 17:49:50 -0300

    feat: create end to end tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+let token: string;
+let taskId: string;
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +18,75 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/users (POST Create user)', async () => {
+    const body = {
+      username: 'test',
+      email: 'email@mail.com',
+      password: 'password',
+    };
+
+    return request(app.getHttpServer()).post('/users').send(body).expect(201);
+  });
+
+  it('/login (POST Login)', async () => {
+    const body = {
+      username: 'test',
+      password: 'password',
+    };
+
+    const response = request(app.getHttpServer())
+      .post('/users/login')
+      .send(body);
+
+    token = (await response).body.token;
+
+    return response.expect(200);
+  });
+
+  it('/tasks (POST Task)', async () => {
+    const body = {
+      title: 'task title',
+      description: 'task description',
+      endDate: '2023-07-20',
+      status: 'PENDING',
+    };
+
+    const response = request(app.getHttpServer())
+      .post('/tasks')
+      .send(body)
+      .set({ Authorization: `Bearer ${token}` });
+
+    return response.expect(201);
+  });
+
+  it('/tasks (GET All Tasks)', async () => {
+    const response = request(app.getHttpServer())
+      .get('/tasks')
+      .set({ Authorization: `Bearer ${token}` });
+
+    taskId = (await response).body[0].id;
+
+    expect((await response).statusCode).toBe(200);
+  });
+
+  it('/tasks (PUT Task)', async () => {
+    const body = {
+      status: 'DONE',
+    };
+
+    const response = request(app.getHttpServer())
+      .put(`/tasks/${taskId}`)
+      .send(body)
+      .set({ Authorization: `Bearer ${token}` });
+
+    expect((await response).statusCode).toBe(204);
+  });
+
+  it('/tasks (DELETE Task)', async () => {
+    const response = request(app.getHttpServer())
+      .delete(`/tasks/${taskId}`)
+      .set({ Authorization: `Bearer ${token}` });
+
+    expect((await response).statusCode).toBe(200);
   });
 });
